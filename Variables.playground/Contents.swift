72304585ec139a2ae235205c148be338cc28236b
Revanth Pendyala <RevanthPendyala@Revanths-MacBook-Air.local>
2018-01-21 13:42:56 -0600

    first commit

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Operators
+
+//Three types: 
+// Unary Binary Ternary:-
+
+
+var fullName =                      "Revanth Chowdary Pendyala"
+
+   // name stored in Memory(RAM)    // String
+
+var imHungry = true
+// Binary works on two
+
+imHungry = !imHungry
+// Unary working on only one (it's an Unary prefix operator)
+
+var feelFull = true
+
+feelFull = imHungry ? true : false
+
+
+
+
